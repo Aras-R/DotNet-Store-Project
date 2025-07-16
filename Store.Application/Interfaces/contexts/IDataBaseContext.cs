d020a0794ed6c97d1baa7ff0a11aebf8d8499341
aras <arasrasoolzadeh@gmail.com>
2025-07-16 22:00:29 +0330

    write operation save in IDataBaseContext interface

--- Store.Application/Interfaces/contexts/IDataBaseContext.cs
+++ Store.Application/Interfaces/contexts/IDataBaseContext.cs
@@ -14,5 +14,11 @@ namespace Store.Application.Interfaces.contexts
          DbSet<User> Users { get; set; }
          DbSet<Role> Roles { get; set; }
          DbSet<UserInRole> UserInRoles { get; set; }
+
+        int SaveChange(bool acceptAllChangesOnSuccess);
+        int SaveChanges();
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken());
+        
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
     }
 }

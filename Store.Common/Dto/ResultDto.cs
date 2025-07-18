e36f5a0e61eb7f5bfb2a6cd1842fb08520cde427
aras <arasrasoolzadeh@gmail.com>
2025-07-18 15:47:57 +0330

    Update "ResultDto" class

--- Store.Common/Dto/ResultDto.cs
+++ Store.Common/Dto/ResultDto.cs
@@ -11,4 +11,10 @@ namespace Store.Common.Dto
         public bool IsSuccess { get; set; }
         public string Message { get; set; }
     }
+    public class ResultDto<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
 }

c8ba99886bc7e19288ba479164439e87073c9dc2
aras <arasrasoolzadeh@gmail.com>
2025-07-30 16:03:49 +0330

    Writing "Home page images" entity for site template

---
+++ Store.Domain/Entities/HomePages/HomePageImages.cs
@@ -0,0 +1,25 @@
+﻿using Store.Domain.Entities.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain.Entities.HomePages
+{
+    public class HomePageImages:BaseEntity
+    {
+        public string Src { get; set; }
+        public string link { get; set; }
+        public ImageLocation  ImageLocation { get; set; }
+    }
+
+    public enum ImageLocation
+    {
+        IconImages= 0,
+        FourImageCenter = 1,
+        TwoImageCenter = 2,
+        CenterFullScreen= 3,
+    }
+
+}
